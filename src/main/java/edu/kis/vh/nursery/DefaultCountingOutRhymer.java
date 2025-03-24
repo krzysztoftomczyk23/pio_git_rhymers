f4598018d026053a9893053dd764ce06b20078b8
Krzysztof Tomczyk <251236@edu.p.lodz.pl>
2025-03-24 10:55:40 +0100

    9.Zmiana widoczności atrybutów klas.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MAX_SIZE = 12;
-    public static final int EMPTY = -1;
-    public static final int IF_EMPTY = -1;
+    private static final int MAX_SIZE = 12;
+    private static final int EMPTY = -1;
+    private static final int IF_EMPTY = -1;
     private final int[] numbers = new int[MAX_SIZE];
 
-    public int totalNumbers = EMPTY;
+    private int totalNumbers = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
